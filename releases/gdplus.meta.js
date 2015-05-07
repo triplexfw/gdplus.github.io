bb0d2c9e8aa8c6224f74351c8840564ab9b292f7
XOR <xor10@users.noreply.github.com>
2015-05-06 21:30:37 -0300

    add version 2.0.0

---
+++ releases/gdplus.meta.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        GD+
+// @description GDPlus - Adiciona facilidades ao GDrive.
+// @include     https://docs.google.com/*
+// @include     https://drive.google.com/*
+// @copyright   XOR
+// @author      XOR
+// @version     2.0.0
+// @license     MIT License (Expat)
+// @grant       unsafeWindow
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getResourceURL
+// @grant       GM_registerMenuCommand
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_addStyle
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @resource    copiaImg https://i.imgur.com/pBYP5Ad.png
+// @homepageURL https://gdplus.github.io/
+// @updateURL   https://gdplus.github.io/releases/gdplus.meta.js
+// @downloadURL https://gdplus.github.io/releases/gdplus.user.js
+// @namespace   4971e63b909612f5598edd6d90012dae
+// @run-at      document-start
+// @icon        https://i.imgur.com/PEIURCu.png
+// ==/UserScript==
