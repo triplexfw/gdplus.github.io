7d9f927235db353b7b3db93a2ef3bf908b425ab8
XOR <xor10@users.noreply.github.com>
2015-05-08 18:23:46 -0300

    version 2.0.1

--- releases/gdplus.meta.js
+++ releases/gdplus.meta.js
@@ -5,7 +5,7 @@
 // @include     https://drive.google.com/*
 // @copyright   XOR
 // @author      XOR
-// @version     2.0.0
+// @version     2.0.1
 // @license     MIT License (Expat)
 // @grant       unsafeWindow
 // @grant       GM_xmlhttpRequest
