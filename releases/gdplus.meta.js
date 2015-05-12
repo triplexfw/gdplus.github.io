4176adddb5e6377f188aa78c31f5163a61286db2
XOR <xor10@users.noreply.github.com>
2015-05-12 15:08:03 -0300

    version 2.1.1 ('no login' fix)

--- releases/gdplus.meta.js
+++ releases/gdplus.meta.js
@@ -5,7 +5,7 @@
 // @include     https://drive.google.com/*
 // @copyright   XOR
 // @author      XOR
-// @version     2.1.0
+// @version     2.1.1
 // @license     MIT License (Expat)
 // @grant       unsafeWindow
 // @grant       GM_xmlhttpRequest
