6002cb7146fc1c8c178ee1b036b6682ae72fe06e
XOR <xor10@users.noreply.github.com>
2015-05-13 13:13:16 -0300

    version 2.1.2 (config fix)

--- releases/gdplus.meta.js
+++ releases/gdplus.meta.js
@@ -5,7 +5,7 @@
 // @include     https://drive.google.com/*
 // @copyright   XOR
 // @author      XOR
-// @version     2.1.1
+// @version     2.1.2
 // @license     MIT License (Expat)
 // @grant       unsafeWindow
 // @grant       GM_xmlhttpRequest
